d7c777b273bf431eafc8c868d21a55a52dc9f894
shaikjunaidmo <junaid.shaik@anywhere.co>
2020-08-08 11:30:14 +0530

    added Chapter-3

--- Chapter-2/loopover_obj.js
+++ Chapter-2/loopover_obj.js
@@ -1,17 +1,19 @@
+//creating an object address
 var address = {
   hno: 76,
   city: "warnagal",
   state: "telangana"
 };
-
+//creating a function that counts the number of properties in the object.
 function countprop(obj) {
   var count = 0;
   for (var key in obj) {
+    // checking whether the property is in the obj or not.
     if (obj.hasOwnProperty(key)) {
       count++;
     }
   }
-  return count;
+  return count; //returing the count.
 }
 
 module.exports = countprop(address);

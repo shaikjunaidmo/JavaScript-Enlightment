a3fea9bdf3fa39d1c2ee15490e3589cda1b081db
shaikjunaidmo <junaid.shaik@anywhere.co>
2020-08-12 19:42:42 +0530

    added chapter 10-15 codes

---
+++ Chapter-14/undefined.js
@@ -0,0 +1,7 @@
+var undef;
+console.log(undef);
+console.log(typeof undef);
+var emptyObj = {};
+console.log(emptyObj.name);
+console.log(typeof emptyObj.name);
+console.log(emptyObj.name in global);
